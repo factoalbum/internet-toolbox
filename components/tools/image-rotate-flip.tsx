dc4a919aa936f28a3302c7041db7aa49b467925c
factoalbum <business.factoalbum@gmail.com>
2026-09-14 13:52:25 +0530

    fix: wire image rotate tool to shared suite

--- components/tools/image-rotate-flip.tsx
+++ components/tools/image-rotate-flip.tsx
@@ -1,1 +1,5 @@
-export { ImageRotateFlip as default } from "./image-edit-suite";
+import ImageEditSuite from "./image-edit-suite";
+
+export default function ImageRotateFlip() {
+  return <ImageEditSuite variant="image-rotate-flip" />;
+}
